6feb0b701981dec02d80b1b154e3fa2534416d07
sploit47 <degtyaralexander@gmail.com>
2025-10-30 19:54:54 +0300

    lab-3

---
+++ src/js/script.js
@@ -0,0 +1,75 @@
+import "/src/sass/style.scss";
+import Swiper from 'swiper';
+import { Autoplay, Pagination } from 'swiper/modules';
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const swiper = new Swiper('.gallery-swiper', {
+  modules: [Autoplay, Pagination],
+  
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  centeredSlides: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: -110,
+        centeredSlides: false,
+    },
+      1920: {
+        slidesPerView: 3,
+          spaceBetween: -30,
+          centeredSlides: false,
+      }
+  },
+});
+
+swiper.on('slideChange', function () {
+  console.log('slide changed');
+});
+
+const menu = document.querySelector('.menu');
+
+document.querySelector('.burger').addEventListener('click', function() {
+  menu.classList.add('menu--active');
+});
+
+const menuClose = document.querySelector('.menu-close');
+
+menuClose.addEventListener('click', function() {
+  menu.classList.remove('menu--active');
+});
+
+const filterButtons = document.querySelectorAll('.filters__filter-item');
+
+filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        filterButtons.forEach(btn => btn.classList.remove('active'));
+
+        button.classList.add('active');
+    });
+});
